b26888498333f67cdad9820bd8daa15ab54f638b
Niels van der Horst <niels1107@gmail.com>
2023-04-22 19:42:53 +0200

    first commit

---
+++ src/pages/RecipeListPage.jsx
@@ -0,0 +1,10 @@
+import { Center, Heading } from '@chakra-ui/react';
+import { data } from '../utils/data';
+
+export const RecipeListPage = () => {
+  return (
+    <Center h="100vh" flexDir="collumn">
+      <Heading>Your Recipe App</Heading>
+    </Center>
+  );
+};
